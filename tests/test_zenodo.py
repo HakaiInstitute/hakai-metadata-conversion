3455699c6e68db3da83b179af2487c01dcfc3fc5
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2024-08-15 10:44:09 -0400

    isort black

--- tests/test_zenodo.py
+++ tests/test_zenodo.py
@@ -1,11 +1,13 @@
-from hakai_metadata_conversion.zenodo import zenodo
-from hakai_metadata_conversion.__main__ import load
 from glob import glob
+
 import pytest
+
+from hakai_metadata_conversion.__main__ import load
+from hakai_metadata_conversion.zenodo import zenodo
+
 
 @pytest.mark.parametrize("file", glob("tests/records/**/*.yaml", recursive=True))
 def test_zenodo_record(file):
     record = load(file, "yaml")
     result = zenodo(record)
     assert result
-    
